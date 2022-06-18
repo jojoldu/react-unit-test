636de77c131fe751f19e8d4b791543040bf334f3
jojoldu <jojoldu@gmail.com>
2022-06-18 23:07:10 +0900

    react-query 환경설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const queryClient = new QueryClient();
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </QueryClientProvider>
   );
 }
-
-export default App;
