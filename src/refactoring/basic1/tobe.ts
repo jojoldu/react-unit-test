6a400f28e3516cd39b0d1bcc0efe16acbc29a543
jojoldu <jojoldu@gmail.com>
2022-12-31 00:21:55 +0900

    기본 예제 1 완성

---
+++ src/refactoring/basic1/tobe.ts
@@ -0,0 +1,22 @@
+import { CompanySelling } from "./type/CompanySelling";
+import { apiSendFee } from "./api/apiSendFee";
+
+export function sendCompanyFees(companySellings: CompanySelling[]) {
+  const companyFees = getCompanyFees(companySellings);
+  for (const companyFee of companyFees) {
+    apiSendFee(companyFee.bankCode, companyFee.fee);
+  }
+}
+
+function getCompanyFees(companySellings: CompanySelling[]) {
+  return companySellings
+    .map(c => getCompanyFee(c))
+    .filter(c => c.fee > 100); // 100원 이상이면 송금하기
+}
+
+function getCompanyFee(companySelling: CompanySelling) {
+  return {
+    fee: companySelling.sellingAmount * companySelling.commission,
+    bankCode: companySelling.bankCode
+  };
+}
---
+++ src/refactoring/basic1/type/CompanySelling.ts
@@ -0,0 +1,5 @@
+export type CompanySelling = {
+  sellingAmount: number;
+  commission: number;
+  bankCode: string;
+}
