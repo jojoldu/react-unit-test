9411cb8cdee7e3f13e0fe3ccef5712e7b61ed934
jojoldu <jojoldu@gmail.com>
2023-01-03 19:46:17 +0900

    basic2 해소

--- src/refactoring/basic2/tobe.ts
+++ src/refactoring/basic2/tobe.ts
@@ -1,9 +1,6 @@
 import { CompanySelling } from '../basic1/type/CompanySelling';
 import { apiSendFee } from '../basic1/api/apiSendFee';
 
-/**
- * 여기서 순수 함수와 부수효과 함수는 각각 몇개씩 존재하는지?
- */
 export function sendCompanyFees(companySellings: CompanySelling[]) {
   const companyFees = getCompanyFees(companySellings);
   for (const companyFee of companyFees) {
@@ -17,8 +14,10 @@ function getCompanyFees(companySellings: CompanySelling[]) {
     .filter((c) => c.fee > 100); // 100원 이상이면 송금하기
 }
 
-function getCompanyFee(companySelling: CompanySelling) {
-  const requestAt = new Date().toISOString();
+function getCompanyFee(
+  companySelling: CompanySelling,
+  requestAt = new Date().toISOString(),
+) {
   return {
     fee: companySelling.sellingAmount * companySelling.commission,
     bankCode: companySelling.bankCode,
