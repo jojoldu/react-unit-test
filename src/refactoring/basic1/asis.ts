16eeb7a85375cbdaaff0f2841a4249a6466f6d1e
jojoldu <jojoldu@gmail.com>
2023-01-08 20:02:48 +0900

    기본 예제

--- src/refactoring/basic1/asis.ts
+++ src/refactoring/basic1/asis.ts
@@ -1,24 +1,13 @@
 import { CompanySelling } from './type/CompanySelling';
 import { apiSendFee } from './api/apiSendFee';
 
-function sendFee(fee: number, companySelling: CompanySelling) {
-  if (fee >= 100) {
-    // 100원 이상이면 송금하기
-    apiSendFee(companySelling.bankCode, fee);
-  }
-}
-
-function sendFeeWithCalculate(companySelling: CompanySelling) {
-  const fee = companySelling.sellingAmount * companySelling.commission;
-  sendFee(fee, companySelling);
-}
-
-function sendFees(companySellings: CompanySelling[]) {
+export function sendCompanyFees(companySellings: CompanySelling[]) {
   for (const companySelling of companySellings) {
-    sendFeeWithCalculate(companySelling);
-  }
-}
+    const fee = companySelling.sellingAmount * companySelling.commission;
 
-export function main(companySellings: CompanySelling[]) {
-  sendFees(companySellings);
+    if (fee >= 100) {
+      // 100원 이상이면 송금하기
+      apiSendFee(companySelling.bankCode, fee);
+    }
+  }
 }
