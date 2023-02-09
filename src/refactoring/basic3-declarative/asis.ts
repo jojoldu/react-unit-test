82318f3a3538f09d69c509f4ac6c2baf42518a4d
jojoldu <jojoldu@gmail.com>
2023-02-09 17:53:02 +0900

    예제3 구체화된 구현 코드 추가

---
+++ src/refactoring/basic3-declarative/asis.ts
@@ -0,0 +1,9 @@
+export function main(e) {
+  const ct = e.currentTarget;
+  const type = ct.dataset.type;
+  const parent = $.closest('.accordion-body', ct);
+  const filtered = map(
+    (el) => el.value,
+    filter((el) => el.checked, $.findAll(`input[data-type="${type}"]`, parent)),
+  ).join(',');
+}
