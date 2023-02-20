310de14f3a3591cae9b40ef2a485806fe5acbd5d
jojoldu <jojoldu@gmail.com>
2023-02-20 21:35:51 +0900

    실전예제 1-3 추가

---
+++ src/refactoring/exam-if/1-3/asis.ts
@@ -0,0 +1,41 @@
+export type VoucherOrder = {
+  carts: Cart[];
+};
+
+export type Cart = {
+  discount: Discount;
+  coupon_id: number;
+};
+
+export type Discount = {
+  dis1: number;
+};
+
+export const voucherOrderDTO = (voucherOrder: VoucherOrder) => {
+  if (!voucherOrder) {
+    return null;
+  }
+
+  let discountOfProducts = 0;
+  let discountOfCoupon = 0;
+
+  voucherOrder.carts.forEach(({ discount, coupon_id }) => {
+    const isDiscountOfCoupon = !!coupon_id;
+
+    let discount1 = 0;
+    if (discount !== null && discount !== undefined) {
+      discount1 = discount.dis1 || 0;
+    }
+
+    if (isDiscountOfCoupon) {
+      discountOfCoupon = discountOfCoupon + discount1;
+    } else {
+      discountOfProducts = discountOfProducts + discount1;
+    }
+  });
+
+  return {
+    discountOfProducts,
+    discountOfCoupon,
+  };
+};
