feab0d3ec6d2c3e00fc0660602a86f5153011b45
jojoldu <jojoldu@gmail.com>
2023-02-28 09:05:36 +0900

    2부 정리중

--- src/refactoring/exam1-side-effect/asis.ts
+++ src/refactoring/exam1-side-effect/asis.ts
@@ -4,6 +4,7 @@ export async function notifyTag(fields: []): Promise<void> {
   const response: Response = await fetch('/api/user/tagName');
   // @ts-ignore
   const { userTagName } = response.json();
+
   if (fields.find(({ tagName: tagName }) => tagName === userTagName)) {
     showNotification(userTagName);
 
