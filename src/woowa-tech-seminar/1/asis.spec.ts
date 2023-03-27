fc805af045239bcc75013d991fd3768fae6b0bc9
jojoldu <jojoldu@gmail.com>
2023-03-27 21:53:49 +0900

    Mocking 테스트 추가

--- src/woowa-tech-seminar/1/asis.spec.ts
+++ src/woowa-tech-seminar/1/asis.spec.ts
@@ -1,7 +1,21 @@
 import { Order } from './asis';
+import { LocalDateTime } from '@js-joda/core';
 
 describe('discount', () => {
   it('일요일에는 주문 금액이 10% 할인된다', () => {
+    const sut = new Order(10_000);
+
+    sut.discount();
+
+    expect(sut.amount).toBe(9_000);
+  });
+
+  it('[Mock] 일요일에는 주문 금액이 10% 할인된다', () => {
+    jest.mock('@js-joda/core');
+    LocalDateTime.now = jest
+      .fn()
+      .mockReturnValue(LocalDateTime.of(2023, 3, 26));
+
     const sut = new Order(10_000);
 
     sut.discount();
