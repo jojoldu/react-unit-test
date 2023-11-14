f1323d8ecf2d66e242baed8aa5d043e5f820faf7
jojoldu <jojoldu@gmail.com>
2023-11-14 18:42:45 +0900

    구현클래스 추가

--- src/image/ImageResizer.ts
+++ src/image/ImageResizer.ts
@@ -1,5 +1,19 @@
-export interface ImageSize {
-  width: number;
-  height: number;
-  maxWidth: number;
+import { ImageSize } from './constants';
+
+class ImageResizer {
+  protected width: number;
+  protected height: number;
+  protected maxWidth: number;
+  protected aspectRatio: number;
+
+  constructor({ width, height, maxWidth }: ImageSize) {
+    this.width = width;
+    this.height = height;
+    this.maxWidth = maxWidth;
+    this.aspectRatio = width / height;
+  }
+
+  resize(): ImageSize {
+    return { width: this.width, height: this.height };
+  }
 }
--- src/image/constants.ts
+++ src/image/constants.ts
@@ -4,4 +4,5 @@ export const MIN_IMAGE_WIDTH = 100;
 export type ImageSize = {
   width: number;
   height: number;
+  maxWidth: number;
 };
